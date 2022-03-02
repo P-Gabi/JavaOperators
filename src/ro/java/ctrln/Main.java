23685fdd7c6eadc9c98be04f466dbabcc73db0be
P-Gabi <paul.gabi98@gmail.com>
2022-03-02 13:10:11 +0200

    adaugat javadoc

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,12 +1,13 @@
 package ro.java.ctrln;
 
 public class Main {
-    //Aceasta metoda(main) porneste o aplicatie java
-    public static void main(/* args este parametrul metodei main */String[] args) {
-        //Aici vrem sa afisam in consola intelliJ un mesaj
-        /*
-        Alt exemplu de comentariu
-         */
+    /**
+     * Metoda 'main' este apelata de JVM si care porneste o aplicatie java.
+     * Putem transmite argumente aditionale prin parametrul <b>@args</b>
+     * @param args Acesta este parametrul prin care trimitem argumente programului nostru
+     * @author Gabriel
+     */
+    public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
 
     }
