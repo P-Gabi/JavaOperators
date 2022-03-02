931e08dd2b60ace524545246f3dd68f87361bfae
P-Gabi <100756751+P-Gabi@users.noreply.github.com>
2022-03-02 12:00:32 +0200

    Delete src directory

--- src/AssignmentOperator.java
+++
@@ -1,9 +0,0 @@
-public class AssignmentOperator {
-    public static void main(String[] args) {
-        //operatorul de asignare este =
-        int variabila = 5;
-        System.out.println("Variabila are valoarea : " + variabila);
-
-
-    }
-}
